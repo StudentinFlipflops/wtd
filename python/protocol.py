0224cb5942d6c1b0c0b0df34415937188a500406
Student in flipflops <adam151710@gmail.com>
2023-08-16 13:00:43 +0200

    created new file for protocol's window

---
+++ python/protocol.py
@@ -0,0 +1,68 @@
+from tkinter import * 
+
+
+
+def add_section(app , monitor , workspace , section , window , n ):
+
+
+
+    section.append([app.get() , monitor.get()  , workspace.get() , 0 ])
+
+    # Each time you press the "name" button, it iterates through the list and generates messages from the beginning .
+    # This code needs an uptade in the future.
+
+    for x in section:
+        Message(window , text=x[0] , width=100).grid(row = n , column=0)
+        Message(window , text=x[1] , width=100).grid(row = n , column=1)
+        Message(window , text=x[2] , width=100).grid(row = n , column=2)
+        
+        Button(window , text="Remove section" , command= lambda: print(x[3]) ).grid(row = n , column=3)
+        n+=1
+
+    app.delete(0 , END)
+    monitor.delete(0 , END)
+    workspace.delete(0 , END)
+    #print(section)
+
+def add_protocol(root):
+
+    add_protocol_window = Toplevel(root)
+    add_protocol_window.title("Second Window")
+
+    #creating a list to collect all data about a specific section. 
+    #The variable "n" represents the row in which the message about the section should be displayed. 
+    section_list= []
+    n= len(section_list)+2
+
+    protocol_name_mess = Message( add_protocol_window , text="Please enter the protocol name:"  , width=250)
+    protocol_name_input = Entry( add_protocol_window  , borderwidth=2)
+
+    section_app         = Message(add_protocol_window , text="App to run:" , width=100)
+    section_monitor     = Message(add_protocol_window , text="monitor:" , width=100)
+    section_workspace   = Message(add_protocol_window , text="workspace:" , width=100)
+
+    section_entry_app           =Entry(add_protocol_window , width=30 , borderwidth= 2)
+    section_entry_monitor       =Entry(add_protocol_window , width=30 , borderwidth= 2)
+    section_entry_workspace     =Entry(add_protocol_window , width=30 , borderwidth= 2)
+    
+    section_add_button          =Button(add_protocol_window , text="Add section" , command= lambda:
+        add_section(section_entry_app , section_entry_monitor , section_entry_workspace , section_list , add_protocol_window , n))
+
+  
+
+
+    protocol_name_mess.grid(row=0 , column=0)
+    protocol_name_input.grid(row=0 , column=1)
+
+
+    
+    section_app.grid(row=1 , column=0)
+    section_monitor.grid(row=1 , column=1)
+    section_workspace.grid(row=1 , column=2)
+
+
+    section_entry_app.grid(row=50 , column=0)
+    section_entry_monitor.grid(row=50 , column=1)
+    section_entry_workspace.grid(row=50 , column=2)
+    section_add_button.grid(row=50 , column=3)
+    
--- python/wtd.py
+++ python/wtd.py
@@ -1,75 +1,12 @@
 from tkinter import * 
+from protocol import add_protocol
 
 root = Tk()
 root.title("WTD")
 root.geometry(f"600x600")
 
-def add_section(app , monitor , workspace , section , window , n ):
 
-
-    section.append([app.get() , monitor.get()  , workspace.get()])
-
-    for x in section:
-        Message(window , text=x[0] , width=100).grid(row = n , column=0)
-        Message(window , text=x[1] , width=100).grid(row = n , column=1)
-        Message(window , text=x[2] , width=100).grid(row = n , column=2)
-        n+=1
-
-    app.delete(0 , END)
-    monitor.delete(0 , END)
-    workspace.delete(0 , END)
-    print(section)
-
-
-
-def add_protocol():
-    add_protocol_window = Toplevel(root)
-    add_protocol_window.title("Second Window")
-
-    section_list= [["c" , "c" , "c"],["c" , "c " , "c"]]
-    n= len(section_list)
-    protocol_name_mess = Message( add_protocol_window , text="Please enter the protocol name:"  , width=250)
-    protocol_name_input = Entry( add_protocol_window  , borderwidth=2)
-
-    section_app         = Message(add_protocol_window , text="App to run:" , width=100)
-    section_monitor     = Message(add_protocol_window , text="monitor:" , width=100)
-    section_workspace   = Message(add_protocol_window , text="workspace:" , width=100)
-
-    section_entry_app           =Entry(add_protocol_window , width=30 , borderwidth= 2)
-    section_entry_monitor       =Entry(add_protocol_window , width=30 , borderwidth= 2)
-    section_entry_workspace     =Entry(add_protocol_window , width=30 , borderwidth= 2)
-    
-    section_add_button          =Button(add_protocol_window , text="Add section" , command= lambda:
-        add_section(section_entry_app , section_entry_monitor , section_entry_workspace , section_list , add_protocol_window , n))
-
-
-
-    protocol_name_mess.grid(row=0 , column=0)
-    protocol_name_input.grid(row=0 , column=1)
-
-
-    
-    section_app.grid(row=1 , column=0)
-    section_monitor.grid(row=1 , column=1)
-    section_workspace.grid(row=1 , column=2)
-
-
-    for x in section_list:
-        Message(add_protocol_window , text=x[0] , width=100).grid(row = n , column=0)
-        Message(add_protocol_window , text=x[1] , width=100).grid(row = n , column=1)
-        Message(add_protocol_window , text=x[2] , width=100).grid(row = n , column=2)
-        n+=1
-        print("done")
-    n=2
-
-    section_entry_app.grid(row=50 , column=0)
-    section_entry_monitor.grid(row=50 , column=1)
-    section_entry_workspace.grid(row=50 , column=2)
-    section_add_button.grid(row=50 , column=3)
-
-
-
-add_button = Button(root , text="add" , padx=10 , pady=10 , command= lambda: add_protocol())
+add_button = Button(root , text="add" , padx=10 , pady=10 , command= lambda: add_protocol(root))
 
 
 add_button.grid(row=0 , column= 0)
@@ -79,7 +16,7 @@ root.mainloop()
 #Do this things in specific order 
 
 
-#TODO: move fun "add_protocol" and "add_section" to new file
+# *** DONE *** #TODO: move fun "add_protocol" and "add_section" to new file
 #TODO: add button "remove section"
 #TODO: add button "save protocol"
 #TODO: write the simmplest way to write sections
